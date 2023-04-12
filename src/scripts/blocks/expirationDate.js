68cb268a613278e5c9a8bacf5e82bcf321b3cfa2
Vo7kov <drainys1234@gmail.com>
2023-04-12 20:04:42 +0300

    Update 2023-04-12T17:04:39.087Z

--- src/scripts/blocks/expirationDate.js
+++
@@ -1,7 +0,0 @@
-const cardExpirartionDateInput = document.getElementById('expirationDatePay');
-
-cardExpirartionDateInput.addEventListener('input', function() {
-  if (this.value.length === 2 && !this.value.includes('/')) {
-    this.value = `${this.value}/`;
-  }
-});
--- src/scripts/blocks/preventPageScroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('hashchange', () => {
-  // eslint-disable-next-line no-mixed-operators,, max-len, no-multi-spaces
-  if (window.location.hash === '#menu' && window.innerWidth < 1280  || window.location.hash === '#buy') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
