02029ea10698bf518af319472ecde4876ab12499
Vo7kov <drainys1234@gmail.com>
2023-04-12 20:04:17 +0300

    katvr landing was added

---
+++ src/scripts/blocks/expirationDate.js
@@ -0,0 +1,7 @@
+const cardExpirartionDateInput = document.getElementById('expirationDatePay');
+
+cardExpirartionDateInput.addEventListener('input', function() {
+  if (this.value.length === 2 && !this.value.includes('/')) {
+    this.value = `${this.value}/`;
+  }
+});
---
+++ src/scripts/main.min.js
@@ -0,0 +1,14 @@
+import './blocks/sliderHeader.js';
+import './blocks/sliderAbout.js';
+
+import './blocks/video.js';
+import './blocks/techButtons.js';
+import './blocks/preventPageScroll.js';
+import './blocks/modal.js';
+import './blocks/accordion.js';
+import './blocks/dropdown.js';
+import './blocks/cardSystemSelect.js';
+import './blocks/expirationDate.js';
+
+import './blocks/formValidation.js';
+import './blocks/formInputshover.js';
