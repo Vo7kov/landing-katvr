d7fe52c9b19311f4ddf4f2ec2c7c02ac4da1e333
Vo7kov <drainys1234@gmail.com>
2023-04-12 20:16:32 +0300

    Update 2023-04-12T17:16:29.716Z

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+import './sliderHeader.js';
+import './sliderAbout.js';
+
+import './video.js';
+import './techButtons.js';
+import './preventPageScroll.js';
+import './modal.js';
+import './accordion.js';
+import './dropdown.js';
+import './cardSystemSelect.js';
+import './expirationDate.js';
+
+import './formValidation.js';
+import './formInputshover.js';
---
+++ scripts/sliderHeader.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-new, no-undef
+new Swiper('.header__slider-container', {
+  loop: true,
+
+  scrollbar: {
+    el: '.header__slider-scrollbar',
+    draggable: true,
+  },
+
+  navigation: {
+    nextEl: '.header__slider-nav-button-next',
+    prevEl: '.header__slider-nav-button-prev',
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
